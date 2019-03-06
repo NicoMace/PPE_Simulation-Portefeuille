c3c98fd31752f691287c76a2d7ca6c97ca5d294f
Mithuran Gajendran <mithurangajendran@MacBook-Pro-de-Mithuran.local>
2019-03-06 18:25:37 +0100

    maj mithuran

--- Python/Strategies/Exposition constante.py
+++ Python/Strategies/Exposition constante.py
@@ -82,9 +82,56 @@ def strat_EC(treshold, portfolio, index, start, Nb_Obs, periode):
         
     return m_PnL, value
 
+
+
+
+        
+def strat_EC_1(treshold, portfolio, index, start, Nb_Obs, periode):
+    m_PnL=[]
+    value=[]
+    investment= portfolio.get_ptf_list_investments()[index] 
+    
+    for jour in range(start, Nb_Obs+start, periode):
+        investment.get_investment_asset().set_asset_price(data.iloc[jour][investment.get_investment_asset().get_asset_ISIN()])
+        lower_qty= treshold// investment.get_investment_asset().get_asset_price()
+        upper_qty=lower_qty+1
+        #print(investment.get_investment_quantity())
+        
+        
+        if investment.get_investment_asset().comp_asset_cost(lower_qty,b1)<investment.get_investment_asset().comp_asset_cost(upper_qty,b1):
+            if lower_qty-investment.get_investment_quantity()<0:
+            
+                portfolio.sell_ptf(index,abs(lower_qty-investment.get_investment_quantity()))
+                print("lol :"+ str(lower_qty-investment.get_investment_quantity()))
+                
+            elif lower_qty-investment.get_investment_quantity()>0:
+                portfolio.buy_ptf(index,abs(lower_qty-investment.get_investment_quantity()))
+                
+        
+        elif investment.get_investment_asset().comp_asset_cost(lower_qty,b1)>investment.get_investment_asset().comp_asset_cost(upper_qty,b1):
+            if upper_qty-investment.get_investment_quantity()<0:
+                portfolio.sell_ptf(index,abs(upper_qty-investment.get_investment_quantity()))
+            
+            elif upper_qty-investment.get_investment_quantity()>0:
+                portfolio.buy_ptf(index,abs(upper_qty-investment.get_investment_quantity()))
+        p1.comp_ptf_PnL()
+        value.append(portfolio.comp_ptf_value())
+        
+        m_PnL.append(p1.get_ptf_PnL())
+        #print("Investment Quantity :"+ str(investment.get_investment_quantity())+" Price night :"+ str(investment.comp_investment_price(b1)))
+        #print("PnL :"+ str(p1.get_ptf_PnL())+"\n")
+        
+    return m_PnL, value
+
+
+
+
+
+
+
     
 
-m_PnL, value=strat_EC(200, p1,0,0,200,1)
+m_PnL, value=strat_EC(500, p1,0,60,65,1)
 
 
 print(np.mean(m_PnL))
