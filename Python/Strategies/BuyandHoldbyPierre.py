c149433b90a1a4b974b21d52ead31b7f998b5847
Whitelord100 <45971273+Whitelord100@users.noreply.github.com>
2019-03-18 17:37:14 +0100

    Maj

--- Python/Strategies/BuyandHoldbyPierre.py
+++ Python/Strategies/BuyandHoldbyPierre.py
@@ -23,40 +23,13 @@ from Classes import *
 
 ### Buy and Hold.
 
-def strat_buy_and_hold_bis(Capital, BrokerName, Start, End, Return, Risk, AssetBasket, DataAsset, DataBroker):
+def strat_buy_and_hold_bis(Capital, Broker, Start, End, Return, Risk, Portfolio, DataAsset, DataBroker):
     
     '''help'''
     
-    ### Initialisations.
-    
-    # Create Broker.
-    Broker = Broker(BrokerName, DataBroker)     #Revoir DataBroker
-    
-    # Create Portfolio.
-    Portfolio = Portfolio(Broker, Return, Risk, Capital)
-    
-    # Create Assets.
-    Assets = []
-    for i in range(AssetBasket):
-        AssetName = AssetBasket[i]
-        Assets += [Stock(AssetName, "€")]
-    
-    # Create Investments.
-    Investments = []
-    for i in Assets:
-        
-        # Asset quantity.
-        AssetQuantity = 0
-        
-        # Investments.
-        Investments += [Investment(i, AssetQuantity, Start, DataAsset)]    #Revoir DataAsset
-    
-    # Add investments in portfolio.
-    for i in range(Investments):
-        Portfolio.add_ptf_investment(Investments[i])
-    
+    ### Initialisations.   
     # Compute initials investment cost.
-    L_Investments = portfolio.get_ptf_list_investments()
+    L_Investments = Portfolio.get_ptf_list_investments()
     InitialInvestmentCost = []
     for investment in L_Investments:
         investment.set_investment_cost(DataAsset.iloc[Start][investment.get_investment_asset().get_asset_ISIN()])
@@ -64,26 +37,25 @@ def strat_buy_and_hold_bis(Capital, BrokerName, Start, End, Return, Risk, AssetB
     
     
     ### Daily processing.
+    PortfolioPnL = []
     Periode = End - Start #Revoir le calcul
     for day in range(Periode):
         
         # For each portfolio line.
-        for index, investment in enumerate(L_Investments):
-            
-    
-    
-            m_PnL=[]
-    for jour in range(Start,Nb_Obs+Start,periode):
-        print("Jour " +str(jour))
-        for investment in L_investments:
-            investment.set_investment_cost(DataAsset.iloc[start][investment.get_investment_asset().get_asset_ISIN()])
-            cout_investment = investment.get_investment_cost()
-            
-            prix_actif= DataAsset.iloc[jour][investment.get_investment_asset().get_asset_ISIN()]
-            investment.get_investment_asset().set_asset_price(prix_actif)
-            portfolio.comp_ptf_PnL()
-            m_PnL.append(portfolio.get_ptf_PnL())
-            
-            print(investment.get_investment_asset().get_asset_ISIN()+": Spot t0 :"+ str(cout_investment) + " Spot :" + str(investment.get_investment_asset().get_asset_price()))
-        print("PnL de :"+ str(portfolio.get_ptf_PnL())+"\n")
-    return m_PnL
+        for investment in L_Investments:
+            # Refresh asset price.
+            AssetPrice = DataAsset.iloc[day][investment.get_investment_asset().get_asset_ISIN()]
+            investment.get_investment_asset().set_asset_price(AssetPrice)
+        
+        # Compute portfolio PnL.
+        Portfolio.comp_ptf_PnL()    # Revoir la methode & les arguments
+        PnL = Portfolio.get_ptf_PnL
+        PortfolioPnL += [PnL]
+
+
+
+
+
+
+
+
